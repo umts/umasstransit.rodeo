246a960e42b2e0dfdde33129419af743fdc869fa
David Faulkenberry <david.faulkenberry@gmail.com>
2016-04-03 18:51:25 -0400

    Add private pub thin config file to shared files

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,5 +6,5 @@ set :repo_url, 'git@github.com:umts/umasstransit.rodeo.git'
 set :rails_env, :production
 set :deploy_via, :copy
 set :rbenv_ruby, '2.3.0'
-set :linked_files, %w(config/database.yml)
+set :linked_files, %w(config/database.yml config/private_pub_thin.yml)
 server '45.55.158.215', user: 'dave', roles: [:app, :web, :db], primary: true
