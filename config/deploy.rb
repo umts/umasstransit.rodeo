3f4e8b7d57d063da451e02fa884ce6cbe3e25897
David Faulkenberry <david.faulkenberry@gmail.com>
2016-04-03 06:20:44 -0400

    Welcome

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,13 +10,11 @@ set :linked_files, %w(config/database.yml)
 server '45.55.158.215', user: 'dave', roles: [:app, :web, :db], primary: true
 
 namespace :deploy do
-
-  after :restart, :clear_cache do
+  after :deploy do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      within release_path do
+        execute 'rackup private_pub.ru -s thin -E production -D'
+      end
     end
   end
 
