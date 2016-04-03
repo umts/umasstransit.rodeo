ed2989b11e81b989f43d94d76bb533c5df7670d4
David Faulkenberry <david.faulkenberry@gmail.com>
2016-04-03 03:19:22 -0400

    Remove Faye tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,18 +9,6 @@ set :rbenv_ruby, '2.3.0'
 set :linked_files, %w(config/database.yml)
 server '45.55.158.215', user: 'dave', roles: [:app, :web, :db], primary: true
 
-namespace :faye do
-  task :start do
-    run "cd #{deploy_to}/current && bundle exec rackup private_pub.ru -s thin -E production -D"
-  end
-  task :stop do
-    run "kill `cat #{deploy_to}/shared/pids/faye.pid` || true"
-  end
-end
-before 'deploy:update_code', 'faye:stop'
-after 'deploy:finalize_update', 'faye: start'
-
-
 namespace :deploy do
 
   after :restart, :clear_cache do
