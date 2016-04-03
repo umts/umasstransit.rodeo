353b6ab32accc64d82652b13918a2765051fa5bc
David Faulkenberry <david.faulkenberry@gmail.com>
2016-04-03 19:15:41 -0400

    Add certs dir symlink to cap shared dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,4 +7,5 @@ set :rails_env, :production
 set :deploy_via, :copy
 set :rbenv_ruby, '2.3.0'
 set :linked_files, %w(config/database.yml config/private_pub_thin.yml)
+set :linked_dirs, %w(certs)
 server '45.55.158.215', user: 'dave', roles: [:app, :web, :db], primary: true
