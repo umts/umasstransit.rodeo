7d18c598bda737586d110c8fc1c31871d4eda916
David Faulkenberry <david.faulkenberry@gmail.com>
2016-04-03 06:21:29 -0400

    Remove useless task

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,14 +8,3 @@ set :deploy_via, :copy
 set :rbenv_ruby, '2.3.0'
 set :linked_files, %w(config/database.yml)
 server '45.55.158.215', user: 'dave', roles: [:app, :web, :db], primary: true
-
-namespace :deploy do
-  after :deploy do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      within release_path do
-        execute 'rackup private_pub.ru -s thin -E production -D'
-      end
-    end
-  end
-
-end
