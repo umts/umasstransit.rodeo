d82ec6a5632d798809be110d1f3a35ee0096a12a
David Faulkenberry <david.faulkenberry@gmail.com>
2016-04-04 17:49:56 -0400

    Allow users to change passwords

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  as :user do
+    get 'users/edit', to: 'devise/registrations#edit', as: :edit_user_registration
+    put 'users', to: 'devise/registrations#update', as: :user_registration           
+  end
+
   root 'participants#welcome'
 
   get  'faye_test', to: 'faye#test', as: :faye_test
