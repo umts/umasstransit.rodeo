de7f529c1412d19067701935164f5b0bc8dcea1f
David Faulkenberry <david.faulkenberry@gmail.com>
2016-04-04 17:57:49 -0400

    Don't send password change confirmation emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable
   validates :name, :email, presence: true, uniqueness: true
+
+  def confirmation_required?
+    false
+  end
 end
