c75936f38d036e0551c6f2cfa10be1b8f838fa9c
Karin Eichelman <keichelman@gmail.com>
2016-04-06 12:12:28 -0400

    create action for buses works

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -1,2 +1,12 @@
 class BusesController < ApplicationController
+  def create
+    Bus.create bus_params
+    redirect_to :back
+  end
+
+  private
+
+  def bus_params
+    params.require(:bus).permit :number
+  end
 end
