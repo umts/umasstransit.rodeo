5fb2efbf73849866b5925b0e54691d4a52d5abaf
Karin Eichelman <keichelman@gmail.com>
2016-04-12 12:19:18 -0400

    javascript now just a couple lines

--- app/assets/javascripts/scoring.js
+++ app/assets/javascripts/scoring.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  $('#score_form').areYouSure();
   $('.scoring').on('click', 'button.increment', function(){
     $(this).siblings('button.increment').prop('disabled', false);
     var field_name = $(this).data('field');
@@ -8,48 +9,11 @@ $(document).ready(function(){
       var type = $(this).data('type');
       if(type === 'minus') value--;
       if(type === 'plus') value++;
-      field.val(value);
+      field.val(value).change();
       var min = field.attr('min');
       var max = field.attr('max');
       if(min && value === parseInt(min)) $(this).prop('disabled', true);
       if(max && value === parseInt(max)) $(this).prop('disabled', true);
     }
   });
-  $(function() {
-    $('#score_form').areYouSure();
-  })
-  $(".score_fields :button").click(function() {
-    setTimeout(function() {
-      $(".score_fields :input[type=number]").each(function(){
-        if ($(this).val() != '0') {
-          var current = $(this).val();
-          $(this).val(current).change();
-          return false;
-        } else {
-          $('#score_form').removeClass('dirty');
-        }
-      });
-    }, 1000);
-  });
 });
-
-/*
-    $(".score_fields :button").click(function() {
-      $(".score_fields :input[type=number]").each(function(){
-        if ($(this).val() != '0') {
-          var currentVal = $(this).val();
-          $(this).val(currentVal).change();
-        } else {
-          $('#score_form').removeClass('dirty');
-        }
-      });
-    });
-  });
-  $('.score_fields :input[type=number]').each(function(){
-    if ($(this).val() != '0') {
-      $('#score_form').addClass('dirty')
-    } else {
-      $('#score_form').removeClass('dirty')
-    }
-  })
-*/
