ffa49c108a58a9721502158cea2e40a281803aad
AkivaGreen <green.akiva@outlook.com>
2016-05-24 11:39:24 -0400

    Seperated score test for add and update into seperate files

--- spec/features/circle_check_score_update_spec.rb
+++ spec/features/circle_check_score_update_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
-describe 'adding a circle check score' do
-  before :each do
-    create :participant
+describe 'updating a circle check score' do 
+
+  before :each do 
+    create :circle_check_score
   end
-  context 'with admin privilege' do
-    it 'adds circle check score' do
+
+
+  context 'with admin privilege' do 
+    it 'updates circle check score' do 
       when_current_user_is :admin
       visit circle_check_scores_url
       fill_in 'circle_check_score_defects_found', with: '4'
@@ -15,8 +18,8 @@ describe 'adding a circle check score' do
       expect(input.value).to eql '4'
     end
   end
-  context 'with circle check scorer privilege' do
-    it 'adds circle check score' do
+  context 'with circle check scorer privilege' do 
+    it 'updates circle check score' do 
       when_current_user_is :circle_check_scorer
       visit circle_check_scores_url
       fill_in 'circle_check_score_defects_found', with: '4'
@@ -26,8 +29,8 @@ describe 'adding a circle check score' do
       expect(input.value).to eql '4'
     end
   end
-  context 'with judge privilege' do
-    it 'will not add a circle check score' do
+  context 'with judge privilege' do 
+    it 'will not update circle check score' do 
       when_current_user_is :judge
       visit circle_check_scores_url
       fill_in 'circle_check_score_defects_found', with: '4'
