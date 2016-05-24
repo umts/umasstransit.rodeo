65dcb5e37ad457f6270b1672571f59b686520197
David Faulkenberry <david.faulkenberry@gmail.com>
2016-05-24 13:48:41 -0400

    Change creator spec to feature test

--- spec/features/recording_maneuver_score_spec.rb
+++ spec/features/recording_maneuver_score_spec.rb
@@ -4,13 +4,20 @@ describe 'recording a maneuver score' do
   let!(:judge) { create :user, :judge }
   let!(:maneuver) { create :maneuver }
   let!(:participant) { create :participant }
-  it 'records the creator' do
+  before :each do
     when_current_user_is judge
     visit new_maneuver_participant_path(maneuver: maneuver.name,
                                         participant: participant.number)
+  end
+  it 'works' do
     expect { click_on 'Save & next' }
       .to change { ManeuverParticipant.count }
       .by 1
-    expect(ManeuverParticipant.last.creator).to eql judge
+  end
+  it 'records the creator' do
+    click_on 'Save & next'
+    when_current_user_is :admin
+    visit maneuver_participant_path(ManeuverParticipant.last)
+    expect(page).to have_text "recorded by #{judge.name}"
   end
 end
