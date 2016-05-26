60d9d6ca72faa651cf5eaa25aba2b6035bd66597
AkivaGreen <green.akiva@outlook.com>
2016-05-26 11:09:13 -0400

    Fixed maneuver participant spec

--- spec/models/maneuver_participant_spec.rb
+++ spec/models/maneuver_participant_spec.rb
@@ -7,70 +7,70 @@ describe ManeuverParticipant do
     end
 
     it 'should go down by 10 for each reverse direction' do
-      expect { record.update reversed_direction: 1 }.to change {
-        record.score
-      }.by(-10)
+      expect { record.update reversed_direction: 1 }
+        .to change { record.score }
+        .by(-10)
     end
 
     it 'should go down by 25 when speed target is not reached' do
       record.maneuver.update! speed_target: 25
-      expect { record.update speed_achieved: false }.to change {
-        record.score
-      }.by(-25)
+      expect { record.update speed_achieved: false }
+        .to change { record.score }
+        .by(-25)
     end
 
     it 'should go down by 25 when made additional stops' do
       record.maneuver.update! counts_additional_stops: true
-      expect { record.update made_additional_stops: true }.to change {
-        record.score
-      }.by(-25)
+      expect { record.update made_additional_stops: true }
+        .to change { record.score }
+        .by(-25)
     end
 
     it 'should go down by 50 when not completed as designed' do
-      expect { record.update completed_as_designed: false }.to change {
-        record.score
-      }.by(-50)
+      expect { record.update completed_as_designed: false }
+        .to change { record.score }
+        .by(-50)
     end
 
     it 'should go down by the sum of each point value multiplied by count' do
-      expect { record.update obstacles_hit: { 1 => 1 } }.to change {
-        record.score
-      }.by(-1)
+      expect { record.update obstacles_hit: { 1 => 1 } }
+        .to change { record.score }
+        .by(-1)
     end
 
     context 'should go down by the positive difference' do
       it 'between each distance and the minimum multiplied by the multiplier' do
-        expect { record.update distances_achieved: { [0, 1] => 1 } }.to change {
-          record.score
-        }.by(-1)
+        expect { record.update distances_achieved: { [0, 1] => 1 } }
+          .to change { record.score }
+          .by(-1)
       end
     end
 
     context 'should not go down by negative difference' do
       it 'between each distance and the minimum multiplied by the multiplier' do
-        expect { record.update distances_achieved: { [1, 1] => 0 } }.to change {
-          record.score
-        }.by(0)
+        expect { record.update distances_achieved: { [1, 1] => 0 } }
+          .to change { record.score }
+          .by(0)
       end
     end
   end
 
   describe 'previous participant' do
     it'finds previous participant' do
-      partip_1 = create :maneuver_participant
-      partip_2 = create :maneuver_participant
+      record_1 = create :maneuver_participant
+      record_2 = create :maneuver_participant
       maneuver = create :maneuver,
-                        maneuver_participants: [partip_1, partip_2]
-      actual = partip_2.participant.number
-      expected = partip_1.participant.id
+                        maneuver_participants: [record_1, record_2]
+      actual = record_2.participant.number
+      expected = record_1.participant.id
       expect(maneuver.previous_participant(actual).id).to be expected
     end
     it'finds previous participant with no arguement' do
-      partip_1 = create :maneuver_participant
-      partip_2 = create :maneuver_participant
+      record_1 = create :maneuver_participant
+      record_2 = create :maneuver_participant
       maneuver = create :maneuver,
-                        maneuver_participants: [partip_1, partip_2]
-      expect(maneuver.previous_participant.id).to be partip_2.participant.id
+                        maneuver_participants: [record_1, record_2]
+      expect(maneuver.previous_participant.id).to be record_2.participant.id
     end
   end
 end
