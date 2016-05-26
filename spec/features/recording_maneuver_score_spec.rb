f422d796fc42a49ebea67ec0ccd37a586cb89a21
Molly O'Neil <mboneil@umass.edu>
2016-05-26 15:34:12 -0400

    Finished creating tests for maneuver participants controller

--- spec/features/recording_maneuver_score_spec.rb
+++ spec/features/recording_maneuver_score_spec.rb
@@ -20,12 +20,47 @@ describe 'recording a maneuver score' do
     visit maneuver_participant_path(ManeuverParticipant.last)
     expect(page).to have_text "recorded by #{judge.name}"
   end
-  #   it 'updates' do
-  #     click_on 'Save & next'
-  #     when_current_user_is :admin
-  #     visit maneuver_participant_path(id: participant.id)
-  #     fill_in
-  #     expect { click_on 'Save & next' }
-  #       .to be {}
-  #   end
+end
+
+describe 'updating a maneuver score' do
+  let!(:maneuver_participant) do
+    create :maneuver_participant, :perfect_score
+  end
+  it 'works' do
+    when_current_user_is :admin
+    visit maneuver_participant_path(maneuver_participant.id)
+    fill_in 'reversed_direction', with: '1'
+    click_on 'Save score'
+    input = find_field :reversed_direction
+    expect(input.value).to eql '1'
+  end
+end
+
+describe 'parsing' do
+  let!(:maneuver) { create :maneuver }
+  let!(:obstacle) { create :obstacle, maneuver: maneuver }
+  let!(:distance_target) { create :distance_target, maneuver: maneuver }
+  let!(:maneuver_participant) do
+    create :maneuver_participant,
+           maneuver: maneuver
+  end
+  it 'obstacles' do
+    when_current_user_is :admin
+    visit maneuver_participant_path(maneuver_participant.id)
+    # binding.pry
+    fill_in "obstacle_#{obstacle.id}", with: '1'
+    click_on 'Save score'
+    input = find_field "obstacle_#{obstacle.id}"
+    expect(input.value).to eql '1'
+  end
+
+  it 'distance target' do
+    when_current_user_is :admin
+    visit maneuver_participant_path(maneuver_participant.id)
+    # binding.pry
+    fill_in "target_#{distance_target.id}", with: '1'
+    click_on 'Save score'
+    input = find_field "target_#{distance_target.id}"
+    expect(input.value).to eql '1'
+  end
 end
