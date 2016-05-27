44c0a3257a03defeb20f511c1de2ff821d37b864
Molly O'Neil <mboneil@umass.edu>
2016-05-27 16:51:19 -0400

    Finished tests for adding and updating a participant

---
+++ spec/features/participants_update_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+describe 'adding a participant' do
+  context 'with master of ceremonies privilege' do
+    it 'adds a participant' do
+      when_current_user_is :master_of_ceremonies
+      visit participants_url
+      fill_in 'participant_name', with: 'Foo Bar'
+      click_on 'Add'
+      expect(page).to have_text 'Participant was successfully created.'
+    end
+  end
+  context 'with admin privilege' do
+    it 'adds a participant' do
+      when_current_user_is :admin
+      visit participants_url
+      fill_in 'participant_name', with: 'Foo Bar'
+      click_on 'Add'
+      expect(page).to have_text 'Participant was successfully created.'
+    end
+  end
+  context 'with judge privilege' do
+    it 'does not add a participant' do
+      when_current_user_is :judge
+      visit participants_url
+      fill_in 'participant_name', with: 'Foo Bar'
+      click_on 'Add'
+      expect(page).to have_text 'You are not authorized to make that action.'
+    end
+  end
+end
+describe 'updating a participant' do
+  context 'with master of ceremonies privilege' do
+    it 'updates a participant' do
+      create :participant, name: 'Foo Bar'
+      when_current_user_is :master_of_ceremonies
+      visit participants_url
+      within ('tr:nth-child(3)') do
+        fill_in :participant_name, with: 'Akiva Green'
+      end
+      click_on 'Save'
+      expect(page).to have_text 'Participant has been updated.'
+    end
+  end
+  context 'with admin privilege' do
+    it 'updates a participant' do
+      create :participant, name: 'Foo Bar'
+      when_current_user_is :admin
+      visit participants_url
+      within ('tr:nth-child(3)') do
+        fill_in :participant_name, with: 'Akiva Green'
+      end
+      click_on 'Save'
+      expect(page).to have_text 'Participant has been updated.'
+    end
+  end
+  context 'with judge privilege' do
+    it 'does not update a participant' do
+      create :participant, name: 'Foo Bar'
+      when_current_user_is :judge
+      visit participants_url
+      within ('tr:nth-child(3)') do
+        fill_in :participant_name, with: 'Akiva Green'
+      end
+      click_on 'Save'
+      expect(page).to have_text 'You are not authorized to make that action.'
+    end
+  end
+end
