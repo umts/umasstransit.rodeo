cc2a930877627904276bb225af1161113586cded
AkivaGreen <green.akiva@outlook.com>
2016-05-28 12:17:18 -0400

    Fixed spacing in maneuver spec file

--- spec/models/maneuver_participant_spec.rb
+++ spec/models/maneuver_participant_spec.rb
@@ -57,8 +57,8 @@ describe ManeuverParticipant do
 
   context 'previous participant' do
     let!(:maneuver) { create :maneuver }
-    let!(:record_1)  { create :maneuver_participant, maneuver: maneuver }
-    let!(:record_2)  { create :maneuver_participant, maneuver: maneuver }
+    let!(:record_1) { create :maneuver_participant, maneuver: maneuver }
+    let!(:record_2) { create :maneuver_participant, maneuver: maneuver }
     it 'finds previous participant' do
       actual = record_2.participant.number
       expect(maneuver.previous_participant(actual)).to eql record_1.participant
@@ -70,8 +70,8 @@ describe ManeuverParticipant do
 
   context 'next participant' do
     let!(:maneuver) { create :maneuver }
-    let!(:record_1)  { create :maneuver_participant, maneuver: maneuver }
-    let!(:record_2)  { create :maneuver_participant, maneuver: maneuver }
+    let!(:record_1) { create :maneuver_participant, maneuver: maneuver }
+    let!(:record_2) { create :maneuver_participant, maneuver: maneuver }
     it 'finds the next participant' do
       actual = record_1.participant.number
       expect(maneuver.next_participant(actual)).to eql record_2.participant
