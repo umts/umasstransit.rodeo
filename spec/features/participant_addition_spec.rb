bcfcf6ed4db55fbcda99ae7bec87de4a8b9ae48a
Molly O'Neil <mboneil@umass.edu>
2016-05-30 11:40:08 -0400

    Adding participant specs

--- spec/features/participant_addition_spec.rb
+++ spec/features/participant_addition_spec.rb
@@ -40,4 +40,34 @@ describe 'adding a participant' do
       expect(page).to have_text 'Name has already been taken'
     end
   end
+  context 'with a unique number and bus number' do
+    it 'will add a participant' do
+      create :bus, number: 'Big Yellow Bus'
+      when_current_user_is :admin
+      visit participants_url
+      fill_in 'participant_name', with: 'Foo Bar'
+      fill_in 'participant_number', with: '1'
+      select('Big Yellow Bus', from: 'participant_bus_id')
+      click_on 'Add'
+      expect(page).to have_text 'Participant was successfully created.'
+    end
+  end
+  context 'with a unique number and no bus number' do
+    it 'will not add a participant' do
+      when_current_user_is :admin
+      visit participants_url
+      fill_in 'participant_name', with: 'Foo Bar'
+      fill_in 'participant_number', with: '1'
+      click_on 'Add'
+      expect(page).to have_text "Bus can't be blank"
+    end
+  end
+  context 'with blank fields' do
+    it 'will not add a participant' do
+      when_current_user_is :admin
+      visit participants_url
+      click_on 'Add'
+      expect(page).to have_text "Name can't be blank"
+    end
+  end
 end
--- spec/features/participant_update_spec.rb
+++ spec/features/participant_update_spec.rb
@@ -37,4 +37,17 @@ describe 'updating a participant' do
       expect(page).to have_text 'You are not authorized to make that action.'
     end
   end
+  context 'with master of ceremonies privilege' do
+    it 'will assign a number' do
+      create :bus, number: 'Big Yellow Bus'
+      when_current_user_is :master_of_ceremonies
+      visit participants_url
+      fill_in 'participant_name', with: 'Foo Bar'
+      click_on 'Add'
+      select('Foo Bar', from: 'id')
+      select('Big Yellow Bus', from: 'bus_id')
+      click_on 'Add to maneuver queue'
+      expect(page).to have_text 'Participant has been added to the queue.'
+    end
+  end
 end
