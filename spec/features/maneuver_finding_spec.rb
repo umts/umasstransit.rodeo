d2889f409388aaeced5bc26ef9a2fa8a4fad99ff
AkivaGreen <green.akiva@outlook.com>
2016-05-31 11:08:19 -0400

    Ran rubocop

--- spec/features/maneuver_finding_spec.rb
+++ spec/features/maneuver_finding_spec.rb
@@ -14,17 +14,17 @@ describe 'finding a participant' do
     create :maneuver_participant,
            :perfect_score, maneuver: maneuver
   end
-  context 'finds the previous' do 
-    context 'when called on first participant' do 
+  context 'finds the previous' do
+    context 'when called on first participant' do
       it 'gets first participant' do
         when_current_user_is :admin
         visit maneuver_participant_path(record_1.id)
         click_link 'Previous participant'
-        expect = 'This is the first participant who completed this maneuver.'
+        expect = 'This is the first participant who completed this maneuver'
         expect(page).to have_text expect
       end
     end
-    context 'when called on non-first participant' do 
+    context 'when called on non-first participant' do
       it 'redirects to previous participant' do
         when_current_user_is :admin
         visit maneuver_participant_path(record_2.id)
@@ -34,7 +34,7 @@ describe 'finding a participant' do
     end
   end
   context 'finds the next' do
-    context 'when called on non-last participant' do 
+    context 'when called on non-last participant' do
       it 'rediects to the next participant' do
         when_current_user_is :admin
         visit maneuver_participant_path(record_1.id)
@@ -42,16 +42,16 @@ describe 'finding a participant' do
         expect(page).to have_text record_2.participant.name
       end
     end
-    context 'when called on last participant' do 
+    context 'when called on last participant' do
       it 'displays a notice' do
         when_current_user_is :admin
         visit maneuver_participant_path(record_3.id)
         click_link 'Next participant'
-        expect = 'There are no more participants in the queue for this maneuver.'
+        expect = 'There are no more participants in the queue for this maneuver'
         expect(page).to have_text expect
       end
     end
-    context 'when called on last completed participant' do 
+    context 'when called on last completed participant' do
       it 'redirects to new maneuver' do
         create :participant
         when_current_user_is :admin
