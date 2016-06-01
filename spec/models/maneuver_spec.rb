47724ac5453bc30228703b846cf53919968417bd
Molly O'Neil <mboneil@umass.edu>
2016-06-01 09:39:08 -0400

    maneuver spec

--- spec/models/maneuver_spec.rb
+++ spec/models/maneuver_spec.rb
@@ -27,4 +27,28 @@ describe Maneuver do
       expect(maneuver.image_path).to eql 'maneuvers/Foo.png'
     end
   end
+  describe 'grouped_obstacles' do
+    context 'with the same point values' do
+      it 'returns an array of obstacles' do
+        maneuver = create :maneuver
+        obstacle_1 = create :obstacle, maneuver: maneuver
+        obstacle_2 = create :obstacle, maneuver: maneuver 
+        expected = { [obstacle_1.point_value, obstacle_1.obstacle_type] =>
+                    [obstacle_1, obstacle_2] }
+        expect(maneuver.grouped_obstacles).to eql expected
+      end
+    end
+    context 'with different point values' do
+      it 'returns an array of obstacles' do
+        maneuver = create :maneuver
+        obstacle_1 = create :obstacle, maneuver: maneuver, point_value: 2
+        obstacle_2 = create :obstacle, maneuver: maneuver, point_value: 4
+        expected = { [obstacle_1.point_value, obstacle_1.obstacle_type] =>
+                    [obstacle_1],
+                    [obstacle_2.point_value, obstacle_2.obstacle_type] =>
+                    [obstacle_2] }
+        expect(maneuver.grouped_obstacles).to eql expected
+      end
+    end
+  end
 end
