f5703d238209deebc4be12bd4b8e0a5882ab61e9
AkivaGreen <green.akiva@outlook.com>
2016-06-07 12:48:09 -0400

    added user and participant controller specs

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper' 
 
-describe UsersController do 
+describe Admin::UsersController do 
+  include Devise::TestHelpers
   describe 'update' do 
-    context 'bad POST #UPDATE' do 
-      it 'displays error message' do 
-        user = create :user
-        
+    context 'bad PUT #UPDATE' do 
+      it 'will not update name or email' do 
+        request.env["HTTP_REFERER"] = admin_users_url
+        user = create :user, admin: true
+        sign_in user
+        attrs = FactoryGirl.attributes_for(:user, name: '')
+        put :update, id: user, user: attrs
+        user.reload
+        expected = ["Name can't be blank"]
+        expect(response).to redirect_to(:admin_users)
+        expect(flash[:errors]).to match(expected)
+      end
+    end
+  end
+end
