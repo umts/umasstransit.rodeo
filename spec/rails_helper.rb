f832ba002d7915a227413e85fb5fb162dec8d4b2
Arta <artarazavi@gmail.com>
2016-06-13 13:30:30 -0400

    more pull request fixes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,6 @@ RSpec.configure do |config|
   config.include Devise::TestHelpers, type: :controller
   config.infer_spec_type_from_file_location!
   config.use_transactional_fixtures = true
-  config.include Warden::Test::Helpers
+  config.include Warden::Test::Helpers, type: :feature
   config.before(:suite) { Warden.test_mode! }
 end
