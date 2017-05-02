3eb98c6dbbd5cac6920847d76b43d6ae5d1d8697
David Faulkenberry <david.faulkenberry@gmail.com>
2017-05-02 11:29:06 -0400

    Fix CodeClimate issue

--- spec/features/overwriting_maneuver_score_spec.rb
+++ spec/features/overwriting_maneuver_score_spec.rb
@@ -7,7 +7,8 @@ describe 'overwriting a maneuver score' do
       when_current_user_is :judge
       visit new_maneuver_participant_path(
         maneuver: record.maneuver.name,
-        participant: record.participant.number)
+        participant: record.participant.number
+      )
     end
     it 'redirects you to the show page' do
       expect(page.current_url).to eql maneuver_participant_url(record)
