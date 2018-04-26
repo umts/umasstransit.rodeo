f3d3f5e691719feca7f2c3e781c2f50735bb5241
Matt Moretti <werebus@gmail.com>
2018-04-26 10:17:13 -0400

    Version Gemfile and config/deploy.rb the same.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+lock '~> 3.4.0'
 
 set :application, 'umasstransit.rodeo'
 set :repo_url, 'git@github.com:umts/umasstransit.rodeo.git'
