a9984f4b4de4a7cf13b2babef4b9fae591970768
Matt Moretti <werebus@gmail.com>
2019-04-11 14:45:22 -0400

    Rails 4.2 to 5.0

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,16 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
+
 require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
- 
+
 module UmasstransitRodeo
   class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
     config.encoding = 'utf-8'
     config.time_zone = 'Eastern Time (US & Canada)'
     config.autoload_paths << Rails.root.join('lib')
