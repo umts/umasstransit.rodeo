b78ed3c9f87fd9ff0de8408704c1eb809e07000f
Matt Moretti <werebus@gmail.com>
2019-04-12 09:49:54 -0400

    Deprecated controller spec method signature

--- spec/controllers/participants_controller_spec.rb
+++ spec/controllers/participants_controller_spec.rb
@@ -6,7 +6,7 @@ describe ParticipantsController do
       it 'tests something' do
         create :participant
         user = create :user
-        get :scoreboard_partial, id: user, sort_order: 'total_score'
+        get :scoreboard_partial, params: { id: user, sort_order: 'total_score' }
         expect(response).to render_template(:_scoreboard_partial)
       end
     end
