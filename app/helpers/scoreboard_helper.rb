405c81f50db57a80de9c07de25186771a9ce22de
Matt Moretti <werebus@gmail.com>
2019-04-18 08:59:15 -0400

    Simplify scoreboard view
    Noticed quite a bit of repeated logic in the scoreboard view. Thought
    this might be a good place for a helper.

--- app/helpers/scoreboard_helper.rb
+++ app/helpers/scoreboard_helper.rb
@@ -7,4 +7,20 @@ module ScoreboardHelper
     else 'btn-secondary'
     end
   end
+
+  def score_cell(record, new:, edit: nil)
+    if record.present?
+      if edit.present? && @can_edit_scores
+        link_to record.score, edit
+      else
+        record.score
+      end
+    else
+      if @can_edit_scores
+        link_to '&mdash;'.html_safe, new
+      else
+        '&mdash;'.html_safe
+      end
+    end
+  end
 end
