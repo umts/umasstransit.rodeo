96cbe8d2bd6e7f39f6afffdca1dc0d085c2f233b
Matt Moretti <werebus@gmail.com>
2019-04-18 13:05:03 -0400

    Wanted a few of these configuration values
    For other tests I'm writing.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,9 +12,10 @@ require 'rack_session_access/capybara'
 
 ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
-  config.include Devise::Test::ControllerHelpers, type: :controller
   config.infer_spec_type_from_file_location!
   config.use_transactional_fixtures = true
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Warden::Test::Helpers, type: :feature
   config.before(:suite) { Warden.test_mode! }
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,32 +2,26 @@
 
 require 'factory_bot'
 require 'simplecov'
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 SimpleCov.start 'rails'
 
 RSpec.configure do |config|
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+  config.example_status_persistence_file_path = Rails.root.join('spec', 'examples.txt')
+
+  config.default_formatter = 'doc' if config.files_to_run.one?
+
   config.before :all do
     FactoryBot.reload
   end
   config.include FactoryBot::Syntax::Methods
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
-end
-
-# Sets current user based on two acceptable values:
-# 1. a symbol name of a user factory trait;
-# 2. a specific instance of User.
-def when_current_user_is(user)
-  current_user =
-    case user
-    when Symbol then create :user, user
-    when User then user
-    when nil then nil
-    else raise ArgumentError, 'Invalid user type'
-    end
-  login_as current_user
 end
