169436c9f5a0f990381bbcf441c3829b9dbfed98
Matt Moretti <werebus@gmail.com>
2020-03-24 14:23:53 -0400

    App upgrade to 5.2

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,9 @@ module UmasstransitRodeo
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
     config.encoding = 'utf-8'
     config.time_zone = 'Eastern Time (US & Canada)'
   end
