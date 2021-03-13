75b195f006977a484c0d7e4a2f1dc25fda0bd155
Matt Moretti <werebus@gmail.com>
2021-03-13 13:22:37 -0500

    Archiver class
    Essentially a subclass of the `ParticipantsController` with some
    modifications to make the render work outside of a Rails request.

---
+++ lib/scoreboard_renderer.rb
@@ -0,0 +1,36 @@
+class ScoreboardRenderer < ParticipantsController
+  module ScoreboardRendererHelper
+    def javascript_include_tag(*sources)
+      options = sources.extract_options!.merge(debug: false)
+      super(*sources, options)
+    end
+
+    def stylesheet_link_tag(*sources)
+      options = sources.extract_options!.merge(debug: false)
+      super(*sources, options)
+    end
+  end
+
+  helper ScoreboardRendererHelper
+
+  class << self
+    def assigns
+      {
+        participants: Participant.scoreboard_order(nil),
+        can_edit_scores: false,
+        maneuvers: Maneuver.order(:sequence_number),
+        scores: ManeuverParticipant.scoreboard_grouping
+      }
+    end
+
+    def render
+      super(:scoreboard, assigns: assigns)
+    end
+  end
+
+  def flash; {}; end
+
+  def protect_against_forgery?; false; end
+
+  def user_signed_in?; false; end
+end
