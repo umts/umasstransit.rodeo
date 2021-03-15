76e7b776daecaee3f6002d1f693c79c9cdbe00b8
Matt Moretti <werebus@gmail.com>
2021-03-15 12:46:11 -0400

    Added nav_item helper
    Also switched to using the User#role? method instead

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def nav_item(path, text = nil, role: nil)
+    return if role.present? && !current_user.role?(role)
+
+    tag.li class: 'nav-item' do
+      link_to path do
+        tag.button class: %w[mx-3 btn btn-outline-primary], type: 'button' do
+          block_given? ? yield : text
+        end
+      end
+    end
+  end
 end
