f1b3d4ede3a70400685f70cefe904c6bfee72623
Matt Moretti <werebus@gmail.com>
2021-04-01 16:20:16 -0400

    Rubocopped scoreboard_service_spec

--- spec/services/scoreboard_service_spec.rb
+++ spec/services/scoreboard_service_spec.rb
@@ -4,37 +4,26 @@ require 'rails_helper'
 
 RSpec.describe ScoreboardService do
   describe '.update' do
-    %i[circle_check_score maneuver_participant
-       onboard_judging participant quiz_score].each do |model|
-      let!(model) { create model }
+    scores = %i[circle_check_score maneuver_participant onboard_judging quiz_score]
+    scores.each { |model| let!(model) { create model } }
+    let!(:participant) { create :participant }
+
+    scores.each do |score|
+      it "broadcasts #{score}s to their update channel" do
+        chanel = "#{score.to_s.camelize}sChannel".constantize
+        expect { described_class.update with: send(score) }
+          .to have_broadcasted_to("#{score}s:update").from_channel(chanel)
+      end
     end
 
-    it 'broadcasts simple objects to their update channel' do
-      expect { described_class.update with: circle_check_score }
-        .to have_broadcasted_to('circle_check_scores:update')
-        .from_channel(CircleCheckScoresChannel)
-
-      expect { described_class.update with: maneuver_participant }
-        .to have_broadcasted_to('maneuver_participants:update')
-        .from_channel(ManeuverParticipantsChannel)
-
-      expect { described_class.update with: onboard_judging }
-        .to have_broadcasted_to('onboard_judgings:update')
-        .from_channel(OnboardJudgingsChannel)
-
-      expect { described_class.update with: quiz_score }
-        .to have_broadcasted_to('quiz_scores:update')
-        .from_channel(QuizScoresChannel)
-    end
-
-    it 'broadcasts Participants to the correct channel' do
+    it 'broadcasts Participants to the correct channel with a default type, update' do
       expect { described_class.update with: participant }
-        .to have_broadcasted_to('participants:update')
-        .from_channel(ParticipantsChannel)
+        .to have_broadcasted_to('participants:update').from_channel(ParticipantsChannel)
+    end
 
+    it 'broadcasts Participants to the correct channel with a specified type' do
       expect { described_class.update with: participant, type: :wibble }
-        .to have_broadcasted_to('participants:wibble')
-        .from_channel(ParticipantsChannel)
+        .to have_broadcasted_to('participants:wibble').from_channel(ParticipantsChannel)
     end
   end
 end
