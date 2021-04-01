1252d1ac3cbebe6d6cda36655d79aae0e132e86e
Matt Moretti <werebus@gmail.com>
2021-04-01 12:36:41 -0400

    Fix spec/channels

--- spec/channels/maneuver_participants_channel_spec.rb
+++ spec/channels/maneuver_participants_channel_spec.rb
@@ -3,9 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe ManeuverParticipantsChannel do
-  it 'streams from "update"' do
-    subscribe
+  before { subscribe }
+
+  it 'subscribes' do
     expect(subscription).to be_confirmed
+  end
+
+  it 'streams from "update"' do
     expect(subscription).to have_stream_from 'maneuver_participants:update'
   end
 end
--- spec/channels/onboard_judgings_channel_spec.rb
+++ spec/channels/onboard_judgings_channel_spec.rb
@@ -3,9 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe OnboardJudgingsChannel do
-  it 'streams from "update"' do
-    subscribe
+  before { subscribe }
+
+  it 'subscribes' do
     expect(subscription).to be_confirmed
+  end
+
+  it 'streams from "update"' do
     expect(subscription).to have_stream_from 'onboard_judgings:update'
   end
 end
