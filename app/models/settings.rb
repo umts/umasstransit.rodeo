bcdbff46c926f46310bd3b30b4c0f48cc90a5a67
Louis Pate <louispate@gmail.com>
2024-01-08 11:49:07 -0700

    enforce uniqueness for singleton in model as well

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Settings < ApplicationRecord
-  validates :singleton_guard, inclusion: { in: [0] }
+  validates :singleton_guard, inclusion: { in: [0] }, uniqueness: true
 
   def self.instance
     first_or_create!(singleton_guard: 0)
