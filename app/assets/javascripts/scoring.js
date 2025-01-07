34d795aec2bbc79c64e3303fcdfd593354a48e1e
Matt Moretti <werebus@gmail.com>
2025-01-07 07:51:21 -0500

    Move button enable/disable to a 'change' listener
    Closes #157

--- app/assets/javascripts/scoring.js
+++ app/assets/javascripts/scoring.js
@@ -2,19 +2,23 @@ $(function(){
   $('#score_form').areYouSure();
 
   $('.scoring').on('click', 'button.increment', function(){
-    var field_name = $(this).data('field');
-    $("[data-field='" + field_name + "']").prop('disabled', false);
-    var field = $('input[type=number]#' + field_name);
+    var field = $("input[type=number]#" + $(this).data('field'));
     if(field){
       var value = parseInt(field.val());
       var type = $(this).data('type');
       if(type === 'minus') value--;
       if(type === 'plus') value++;
       field.val(value).trigger('change');
-      var min = field.attr('min');
-      var max = field.attr('max');
-      if(value === parseInt(min)) $(this).prop('disabled', true);
-      if(value === parseInt(max)) $(this).prop('disabled', true);
     }
+  });
+
+  $('.scoring').on('change', 'input[type=number]', function(){
+    var buttons = $('button.increment[data-field=' + $(this).attr('id') + ']');
+    buttons.prop('disabled', false);
+    var value = parseInt($(this).val());
+    var min = parseInt($(this).attr('min'));
+    var max = parseInt($(this).attr('max'));
+    if(value <= min) buttons.filter('[data-type=minus]').prop('disabled', true);
+    if(value >= max) buttons.filter('[data-type=plus]').prop('disabled', true);
   });
 });
