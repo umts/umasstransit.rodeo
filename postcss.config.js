dd1eae7a06c22148fc8a8a7840e011c2814dc9b9
Matt Moretti <werebus@gmail.com>
2025-01-08 14:04:51 -0500

    Silence @import warnings, add browserslist file

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   parser: 'postcss-scss',
   plugins: {
-    '@csstools/postcss-sass': {includePaths: ['node_modules'], quietDeps: true},
+    '@csstools/postcss-sass': {
+      includePaths: ['node_modules'],
+      quietDeps: true,
+      silenceDeprecations: ['import']
+    },
     autoprefixer: {}
   }
 };
