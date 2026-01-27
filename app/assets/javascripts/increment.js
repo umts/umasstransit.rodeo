c3bd75d777a0824d9e97aa2ed813187617ee9383
Louis Pate <louispate@gmail.com>
2026-01-27 14:14:41 -0500

    Resolve lint & fix minor bug
    Button wouldn't increment if you clicked the icon within the button, but
    would if you clicked outside of the icon

--- app/assets/javascripts/increment.js
+++ app/assets/javascripts/increment.js
@@ -1,23 +1,23 @@
 $(() => {
-  $('.increment').on('click', 'button', function () {
-    const field = $(this).parent().siblings(':input');
+  $('.increment').on('click', 'button', (e) => {
+    const field = $(e.target).closest(':button').parent().siblings('input');
     if (field) {
-      var value = parseInt(field.val());
-      const action = $(this).data('action');
+      let value = parseInt(field.val());
+      const action = $(e.target).closest(':button').data('action');
       if (action === 'decrement') value--;
       if (action === 'increment') value++;
       field.val(value).trigger('change');
     }
   });
 
-  $('.increment').on('change', 'input[type=number]', function () {
-    const buttons = $(this).siblings().children(':button');
+  $('.increment').on('change', 'input[type=number]', (e) => {
+    const buttons = $(e.target).siblings().children(':button');
     buttons.prop('disabled', false);
-    const value = parseInt($(this).val());
-    const min = parseInt($(this).attr('min'));
-    const max = parseInt($(this).attr('max'));
-    if(value <= min) buttons.filter('[data-action=decrement]').prop('disabled', true);
-    if(value >= max) buttons.filter('[data-action=increment]').prop('disabled', true);
+    const value = parseInt($(e.target).val());
+    const min = parseInt($(e.target).attr('min'));
+    const max = parseInt($(e.target).attr('max'));
+    if (value <= min) buttons.filter('[data-action=decrement]').prop('disabled', true);
+    if (value >= max) buttons.filter('[data-action=increment]').prop('disabled', true);
   });
 
   $('.increment input').trigger('change');
