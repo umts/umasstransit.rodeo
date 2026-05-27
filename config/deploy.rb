14c451b1f751bf84a8d65c6ef2aa01185bb4348c
Matt Moretti <werebus@gmail.com>
2026-05-27 18:01:28 -0400

    Add production credentials

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,10 @@ set :branch, 'main'
 
 set :deploy_to, '/srv/rodeo/'
 
-append :linked_files, 'config/database.yml', 'config/roadeo.yml'
+append :linked_files,
+       'config/database.yml',
+       'config/roadeo.yml',
+       'config/credentials/production.key'
 append :linked_dirs, 'log', 'tmp/pids'
 
 set :bundle_version, 4
