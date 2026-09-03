1b69bf6b068e60bc5c93ecfd66ddf6f261e0512c
benmelz <ben@melz.me>
2026-09-03 13:11:00 -0400

    rubocop -A

--- app/helpers/scoring_helper.rb
+++ app/helpers/scoring_helper.rb
@@ -3,7 +3,7 @@
 module ScoringHelper
   # This is good advice generally, but in this case we want the method
   # signature to be the same as `check_box_tag`.
-  # rubocop:disable Style/OptionalBooleanParameter
+  # rubocop:disable-next Style/OptionalBooleanParameter
   def giant_check_box_tag(name, value = '1', checked = false, options = {})
     tag.div(class: 'd-inline-block btn-group-toggle checkbox',
             data: { toggle: 'buttons' }) do
@@ -13,7 +13,6 @@ module ScoringHelper
       end
     end
   end
-  # rubocop:enable Style/OptionalBooleanParameter
 
   def number_field_tag_with_buttons(name, value = nil, options = {})
     if (range = options.delete(:in) || options.delete(:within))
